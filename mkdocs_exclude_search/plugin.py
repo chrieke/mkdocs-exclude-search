5aaef29ddbb595cbf8b2508b004de3135f2af496
Christoph Rieke <christoph.rieke@up42.com>
2020-10-12 22:23:40 +0200

    working implementation

--- mkdocs_exclude_search/plugin.py
+++ mkdocs_exclude_search/plugin.py
@@ -3,24 +3,19 @@ import sys
 from timeit import default_timer as timer
 from datetime import datetime, timedelta
 import json
+from pathlib import Path
 
 from .lyrics import random_lyrics
 
 from mkdocs import utils as mkdocs_utils
 from mkdocs.config import config_options, Config
 from mkdocs.plugins import BasePlugin
+from mkdocs.contrib.search.search_index import SearchIndex
+#from mkdocs.contrib.search import SearchPlugin
 
 class ExcludeSearch(BasePlugin):
     """
-    Excludes files from the search index.
-
-    https://github.com/mkdocs/mkdocs/issues/773
-
-    mkdocs/mkdocs/commands/build.py
-    Line 251
-    search_index.add_entry_from_context(
-
-    Check variable and remove.
+    Excludes selected nav chapters from the search index.
     """
     config_scheme = (
         ('chapters', config_options.Type((str, list), default=None)),
@@ -31,22 +26,14 @@ class ExcludeSearch(BasePlugin):
         self.total_time = 0
 
     def on_post_build(self, config):
-        chapters = self.config['chapters']
-        if chapters is not None:
-            # read json
-            with open('path_to_file/person.json') as f:
-                data = json.load(f)
-
-            # replace
-
-            # write json
-            # with open('person.txt', 'w') as json_file:
-            #     json.dump(person_dict, json_file)
-
+        if "search" in config["plugins"]:
+            exclude_chapters = self.config['chapters']
+            if exclude_chapters is not None:
+                search_index_fp = Path(config.data["site_dir"]) / "search/search_index.json"
+                with open(search_index_fp, "r") as f:
+                    search_index = json.load(f)
+                included_chapters = [c for c in search_index["docs"] if c["title"] not in exclude_chapters]
+                search_index["docs"] = included_chapters
+                with open(search_index_fp, 'w') as f:
+                    json.dump(search_index, f)
         return config
-    # def on_page_markdown(self, markdown, page, config, site_nav):
-    #
-    #     # hello dolly example https://github.com/fmaida/hello-dolly-mkdocs-plugin
-    #     markdown = markdown.replace("{{dolly}}", random_lyrics())
-    #     return markdown
-
