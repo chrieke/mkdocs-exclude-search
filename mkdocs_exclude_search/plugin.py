6250f36023f7e9ca329f986117ae0832a23a2a4a
Christoph Rieke <christoph.rieke@up42.com>
2020-10-12 22:25:24 +0200

    black formatting

--- mkdocs_exclude_search/plugin.py
+++ mkdocs_exclude_search/plugin.py
@@ -1,25 +1,16 @@
-import os
-import sys
-from timeit import default_timer as timer
-from datetime import datetime, timedelta
 import json
 from pathlib import Path
 
-from .lyrics import random_lyrics
-
-from mkdocs import utils as mkdocs_utils
-from mkdocs.config import config_options, Config
+from mkdocs.config import config_options
 from mkdocs.plugins import BasePlugin
-from mkdocs.contrib.search.search_index import SearchIndex
-#from mkdocs.contrib.search import SearchPlugin
+
 
 class ExcludeSearch(BasePlugin):
     """
     Excludes selected nav chapters from the search index.
     """
-    config_scheme = (
-        ('chapters', config_options.Type((str, list), default=None)),
-    )
+
+    config_scheme = (("chapters", config_options.Type((str, list), default=None)),)
 
     def __init__(self):
         self.enabled = True
@@ -27,13 +18,19 @@ class ExcludeSearch(BasePlugin):
 
     def on_post_build(self, config):
         if "search" in config["plugins"]:
-            exclude_chapters = self.config['chapters']
+            exclude_chapters = self.config["chapters"]
             if exclude_chapters is not None:
-                search_index_fp = Path(config.data["site_dir"]) / "search/search_index.json"
+                search_index_fp = (
+                    Path(config.data["site_dir"]) / "search/search_index.json"
+                )
                 with open(search_index_fp, "r") as f:
                     search_index = json.load(f)
-                included_chapters = [c for c in search_index["docs"] if c["title"] not in exclude_chapters]
+                included_chapters = [
+                    c
+                    for c in search_index["docs"]
+                    if c["title"] not in exclude_chapters
+                ]
                 search_index["docs"] = included_chapters
-                with open(search_index_fp, 'w') as f:
+                with open(search_index_fp, "w") as f:
                     json.dump(search_index, f)
         return config
--- setup.py
+++ setup.py
@@ -2,35 +2,31 @@ from setuptools import setup, find_packages
 
 
 setup(
-    name='mkdocs-exclude-search',
-    version='0.1.0',
-    packages=['mkdocs_exclude_search'],
-    description='A Mkdocs plugin that lets you exclude files from the search index.',
-    long_description='',
-    keywords='mkdocs',
-    url='',
-    author='Christoph Rieke',
-    author_email='christoph.k.rieke@gmail.com',
-    license='MIT',
-    python_requires='>=2.7',
-    install_requires=[
-        'mkdocs>=1.0.4'
-    ],
+    name="mkdocs-exclude-search",
+    version="0.1.0",
+    packages=["mkdocs_exclude_search"],
+    description="A Mkdocs plugin that lets you exclude files from the search index.",
+    long_description="",
+    keywords="mkdocs",
+    url="",
+    author="Christoph Rieke",
+    author_email="christoph.k.rieke@gmail.com",
+    license="MIT",
+    python_requires=">=2.7",
+    install_requires=["mkdocs>=1.0.4"],
     classifiers=[
-        'Development Status :: 4 - Beta',
-        'Intended Audience :: Developers',
-        'Intended Audience :: Information Technology',
-        'License :: OSI Approved :: MIT License',
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 3 :: Only',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7'
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "Intended Audience :: Information Technology",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3 :: Only",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
     ],
     entry_points={
-        'mkdocs.plugins': [
-            'exclude-search = mkdocs_exclude_search:ExcludeSearch'
-        ]
-    }
+        "mkdocs.plugins": ["exclude-search = mkdocs_exclude_search:ExcludeSearch"]
+    },
 )
