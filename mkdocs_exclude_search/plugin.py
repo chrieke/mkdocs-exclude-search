1d9b11595b7707cabf38ab266385d4eedc4ae397
Christoph Rieke <christoph.rieke@up42.com>
2020-10-13 02:33:28 +0200

    add include pattern

--- mkdocs_exclude_search/plugin.py
+++ mkdocs_exclude_search/plugin.py
@@ -1,6 +1,7 @@
 import json
 from pathlib import Path
 import logging
+
 from mkdocs.config import config_options
 from mkdocs.plugins import BasePlugin
 from mkdocs.utils import warning_filter
@@ -23,7 +24,10 @@ class ExcludeSearch(BasePlugin):
     Excludes selected nav chapters from the search index.
     """
 
-    config_scheme = (("files", config_options.Type((str, list), default=None)),)
+    config_scheme = (
+        ("exclude", config_options.Type((str, list), default=None)),
+        ("ignore", config_options.Type((str, list), default=None)),
+    )
 
     def __init__(self):
         self.enabled = True
@@ -36,10 +40,14 @@ class ExcludeSearch(BasePlugin):
                 "plugin is deactivated!"
             )
         else:
-            exclude_files = self.config["files"]
-            if exclude_files is not None:
+            to_exclude = self.config["exclude"]
+            to_ignore = self.config["ignore"]
+            if to_exclude is not None:
                 # TODO: Other suffixes ipynb etc., more robust
-                exclude_files = [f.replace(".md", "") for f in exclude_files]
+                to_exclude = [f.replace(".md", "") for f in to_exclude]
+                if to_ignore is not None:
+                    to_ignore = [f.replace(".md", "") for f in to_ignore]
+
                 search_index_fp = (
                     Path(config.data["site_dir"]) / "search/search_index.json"
                 )
@@ -53,11 +61,13 @@ class ExcludeSearch(BasePlugin):
                     else:
                         rec_main_name, rec_subchapter = rec["location"].split("/")[-2:]
 
-                        if (
-                            rec_main_name + rec_subchapter not in exclude_files
-                            and not rec_main_name in exclude_files # Also ignore subchapters of excluded main files
-                        ):
-                            print(rec["location"])
+                        if rec_main_name + rec_subchapter in to_ignore:
+                            included_records.append(rec)
+                            print("ignored", rec["location"])
+                        elif (rec_main_name not in to_exclude) and (
+                            rec_main_name + rec_subchapter not in to_exclude
+                        ):  # Also ignore subchapters of excluded main files
+                            print("included", rec["location"])
                             included_records.append(rec)
 
                 search_index["docs"] = included_records
