989fcf77d86c42540049a9f944e8d5b8ed92e9e2
Christoph Rieke <christoph.k.rieke@gmail.com>
2020-12-24 04:19:19 +0100

    rmv tests

--- tests/test-mkdocs_exclude_search.py
+++
@@ -1,6 +0,0 @@
-import markdown
-import pytest
-
-from mkdocstrings.extension import MkdocstringsExtension
-from mkdocstrings.handlers.base import Handlers
-from mkdocstrings.references import fix_refs, relative_url
