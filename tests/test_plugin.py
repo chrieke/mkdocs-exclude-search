c695b7f4b8f5f97f4a6d1c59563621c451f4f136
Christoph Rieke <christoph.k.rieke@gmail.com>
2021-02-13 18:35:10 +0100

    simplify tests

--- tests/test_plugin.py
+++ tests/test_plugin.py
@@ -1,21 +1,94 @@
-"""Tests for the plugin module."""
-
-import sys
+from pathlib import Path
+import json
 
 import pytest
-from mkdocs.commands.build import build
-from mkdocs.config.base import load_config
 
 from mkdocs_exclude_search.plugin import ExcludeSearch
 
 
 CONFIG = {"plugins": ["search"]}
-TO_EXCLUDE = ["abc"]
-EXCLUDE_TAGS = False
+TO_EXCLUDE = [
+    "chapter_exclude_all.md",
+    "chapter_exclude_heading2.md#single-header-chapter_exclude_heading2-bbex",
+    "dir_chapter_exclude_all.md",
+    "dir_chapter_ignore_heading3.md",
+    "all_dir/*.md",
+    "all_dir_sub/all_dir_sub2/*.md",
+]
+
+RESOLVED_EXCLUDED_RECORDS = [
+    "chapter_exclude_all",
+    "chapter_exclude_heading2#single-header-chapter_exclude_heading2-bbex",
+    "dir_chapter_exclude_all",
+    "dir_chapter_ignore_heading3",
+    "all_dir",
+    "all_dir_suball_dir_sub2",
+]
 
 TO_IGNORE = [
     "dir_chapter_ignore_heading3.md#dir-single-header-dir_chapter_ignore_heading3-ccin",
     "all_dir_ignore_heading1.md#alldir-header-all_dir_ignore_heading1-aain",
+]
+
+RESOLVED_IGNORED_CHAPTERS = [
+    "dir_chapter_ignore_heading3#dir-single-header-dir_chapter_ignore_heading3-ccin",
+    "all_dir_ignore_heading1#alldir-header-all_dir_ignore_heading1-aain",
+    "dir_chapter_ignore_heading3",
+    "all_dir_ignore_heading1",
+]
+
+EXCLUDE_TAGS = False
+
+INCLUDED_RECORDS = [
+    {"location": "", "text": "Index Hello, hello", "title": "index"},
+    {"location": "#index", "text": "Hello, hello", "title": "Index"},
+    {
+        "location": "chapter_exclude_heading2/",
+        "text": "single chapter_exclude_heading2 Header Ain single header chapter_exclude_heading2 AAin single text chapter_exclude_heading2 AAin single header chapter_exclude_heading2 BBex single text chapter_exclude_heading2 BBex",
+        "title": "chapter_exclude_heading2",
+    },
+    {
+        "location": "chapter_exclude_heading2/#single-chapter_exclude_heading2-header-ain",
+        "text": "",
+        "title": "single chapter_exclude_heading2 Header Ain",
+    },
+    {
+        "location": "chapter_exclude_heading2/#single-header-chapter_exclude_heading2-aain",
+        "text": "single text chapter_exclude_heading2 AAin",
+        "title": "single header chapter_exclude_heading2 AAin",
+    },
+    {
+        "location": "all_dir/all_dir_ignore_heading1/",
+        "text": "alldir Header all_dir_ignore_heading1 Aex alldir header all_dir_ignore_heading1 AAin alldir text all_dir_ignore_heading1 AAin alldir header all_dir_ignore_heading1 BBex alldir text all_dir_ignore_heading1 BBex",
+        "title": "all_dir_ignore_heading1",
+    },
+    {
+        "location": "all_dir/all_dir_ignore_heading1/#alldir-header-all_dir_ignore_heading1-aain",
+        "text": "alldir text all_dir_ignore_heading1 AAin",
+        "title": "alldir header all_dir_ignore_heading1 AAin",
+    },
+    {
+        "location": "dir/dir_chapter_ignore_heading3/",
+        "text": "dir single Header dir_chapter_ignore_heading3 Aex dir single header dir_chapter_ignore_heading3 AAex dir single text dir_chapter_ignore_heading3 AAex dir single header dir_chapter_ignore_heading3 CCin dir single text dir_chapter_ignore_heading3 CCin",
+        "title": "dir_chapter_ignore_heading3",
+    },
+    {
+        "location": "dir/dir_chapter_ignore_heading3/#dir-single-header-dir_chapter_ignore_heading3-ccin",
+        "text": "dir single text dir_chapter_ignore_heading3 CCin",
+        "title": "dir single header dir_chapter_ignore_heading3 CCin",
+    },
+    {
+        "location": "tags.html",
+        "text": "Contents grouped by tag testing Welcome unimportant Welcome",
+        "title": "Tags",
+    },
+    {
+        "location": "tags.html#contents-grouped-by-tag",
+        "text": "",
+        "title": "Contents grouped by tag",
+    },
+    {"location": "tags.html#testing", "text": "Welcome", "title": "testing"},
+    {"location": "tags.html#unimportant", "text": "Welcome", "title": "unimportant"},
 ]
 
 
@@ -43,13 +116,30 @@ def test_check_config_raises_no_exclusion():
         )
 
 
+def test_resolve_excluded_records():
+    resolved_excluded_records = ExcludeSearch.resolve_excluded_records(
+        to_exclude=TO_EXCLUDE
+    )
+    assert resolved_excluded_records == RESOLVED_EXCLUDED_RECORDS
+
+
 def test_resolve_ignored_chapters():
     resolved_ignored_chapters = ExcludeSearch.resolve_ignored_chapters(
         to_ignore=TO_IGNORE
     )
-    assert resolved_ignored_chapters == [
-        "dir_chapter_ignore_heading3#dir-single-header-dir_chapter_ignore_heading3-ccin",
-        "all_dir_ignore_heading1#alldir-header-all_dir_ignore_heading1-aain",
-        "dir_chapter_ignore_heading3",
-        "all_dir_ignore_heading1",
-    ]
+    assert resolved_ignored_chapters == RESOLVED_IGNORED_CHAPTERS
+
+
+def test_select_records():
+    _location_ = Path(__file__).resolve().parent
+    with open(_location_.joinpath("mock_data/mock_search_index.json"), "r") as f:
+        mock_search_index = json.load(f)
+
+    included_records = ExcludeSearch.select_included_records(
+        search_index=mock_search_index,
+        to_exclude=RESOLVED_EXCLUDED_RECORDS,
+        to_ignore=RESOLVED_IGNORED_CHAPTERS,
+        exclude_tags=EXCLUDE_TAGS,
+    )
+    assert isinstance(included_records, list)
+    assert included_records == INCLUDED_RECORDS
