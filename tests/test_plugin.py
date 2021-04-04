29e9a4585ccb936f2381f5bc3f4efce6e0ddccd3
Christoph Rieke <christoph.k.rieke@gmail.com>
2021-04-04 18:30:23 +0200

    move test globals to separate module

--- tests/test_plugin.py
+++ tests/test_plugin.py
@@ -4,102 +4,15 @@ import json
 import pytest
 
 from .context import ExcludeSearch
-
-
-CONFIG = {"plugins": ["search"]}
-TO_EXCLUDE = [
-    "chapter_exclude_all.md",
-    "chapter_exclude_heading2.md#single-header-chapter_exclude_heading2-bbex",
-    "dir_chapter_exclude_all.md",
-    "dir_chapter_ignore_heading3.md",
-    "all_dir/*.md",
-    "all_dir_sub/all_dir_sub2/*.md",
-]
-
-RESOLVED_EXCLUDED_RECORDS = [
-    ("chapter_exclude_all.md", None),
-    ("chapter_exclude_heading2.md", "single-header-chapter_exclude_heading2-bbex"),
-    ("dir_chapter_exclude_all.md", None),
-    ("dir_chapter_ignore_heading3.md", None),
-    ("all_dir/*.md", None),
-    ("all_dir_sub/all_dir_sub2/*.md", None),
-]
-
-TO_IGNORE = [
-    "dir_chapter_ignore_heading3.md#dir-single-header-dir_chapter_ignore_heading3-ccin",
-    "all_dir_ignore_heading1.md#alldir-header-all_dir_ignore_heading1-aain",
-]
-
-RESOLVED_IGNORED_CHAPTERS = [
-    (
-        "dir_chapter_ignore_heading3.md",
-        "dir-single-header-dir_chapter_ignore_heading3-ccin",
-    ),
-    ("all_dir_ignore_heading1.md", "alldir-header-all_dir_ignore_heading1-aain"),
-    ("dir_chapter_ignore_heading3.md", None),
-    ("all_dir_ignore_heading1.md", None),
-]
-
-EXCLUDE_TAGS = False
-
-INCLUDED_RECORDS = [
-    {"location": "", "text": "Index Hello, hello", "title": "index"},
-    {"location": "#index", "text": "Hello, hello", "title": "Index"},
-    {
-        "location": "chapter_exclude_heading2/",
-        "text": "single chapter_exclude_heading2 Header Ain single header chapter_exclude_"
-        "heading2 AAin single text chapter_exclude_heading2 AAin single header "
-        "chapter_exclude_heading2 BBex single text chapter_exclude_heading2 BBex",
-        "title": "chapter_exclude_heading2",
-    },
-    {
-        "location": "chapter_exclude_heading2/#single-chapter_exclude_heading2-header-ain",
-        "text": "",
-        "title": "single chapter_exclude_heading2 Header Ain",
-    },
-    {
-        "location": "chapter_exclude_heading2/#single-header-chapter_exclude_heading2-aain",
-        "text": "single text chapter_exclude_heading2 AAin",
-        "title": "single header chapter_exclude_heading2 AAin",
-    },
-    {
-        "location": "all_dir/all_dir_ignore_heading1/",
-        "text": "alldir Header all_dir_ignore_heading1 Aex alldir header all_dir_ignore_"
-        "heading1 AAin alldir text all_dir_ignore_heading1 AAin alldir header "
-        "all_dir_ignore_heading1 BBex alldir text all_dir_ignore_heading1 BBex",
-        "title": "all_dir_ignore_heading1",
-    },
-    {
-        "location": "all_dir/all_dir_ignore_heading1/#alldir-header-all_dir_ignore_heading1-aain",
-        "text": "alldir text all_dir_ignore_heading1 AAin",
-        "title": "alldir header all_dir_ignore_heading1 AAin",
-    },
-    {
-        "location": "dir/dir_chapter_ignore_heading3/",
-        "text": "dir single Header dir_chapter_ignore_heading3 Aex dir single header "
-        "dir_chapter_ignore_heading3 AAex dir single text dir_chapter_ignore_heading3 "
-        "AAex dir single header dir_chapter_ignore_heading3 CCin dir single text "
-        "dir_chapter_ignore_heading3 CCin",
-        "title": "dir_chapter_ignore_heading3",
-    },
-    {
-        "location": "dir/dir_chapter_ignore_heading3/#dir-single-header-dir_chapter_ignore_heading3-ccin",
-        "text": "dir single text dir_chapter_ignore_heading3 CCin",
-        "title": "dir single header dir_chapter_ignore_heading3 CCin",
-    },
-    {
-        "location": "tags.html",
-        "text": "Contents grouped by tag testing Welcome unimportant Welcome",
-        "title": "Tags",
-    },
-    {
-        "location": "tags.html#contents-grouped-by-tag",
-        "text": "",
-        "title": "Contents grouped by tag",
-    },
-    {"location": "tags.html#testing", "text": "Welcome", "title": "testing"},
-    {"location": "tags.html#unimportant", "text": "Welcome", "title": "unimportant"},
-]
+from .globals import (
+    CONFIG,
+    TO_EXCLUDE,
+    RESOLVED_EXCLUDED_RECORDS,
+    TO_IGNORE,
+    RESOLVED_IGNORED_CHAPTERS,
+    EXCLUDE_TAGS,
+    INCLUDED_RECORDS,
+)
 
 
 def test_check_config():
